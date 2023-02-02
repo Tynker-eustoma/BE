a181e1a1d45766d9a1f52d44c2e8eb18c8e2baa0
dikyardiyanto17 <dikyardiyanto17@gmail.com>
2023-02-02 19:53:37 +0700

    Updated test with and without access token in game test

--- _test_/game.test.js
+++ _test_/game.test.js
@@ -12,13 +12,15 @@ describe("Category", () => {
         it("Should fetch category", () => {
             return request(app)
                 .get("/category")
+                .set("access_token", token)
                 .then((response) => {
                     expect(response.status).toBe(200)
                     expect(response.body).toHaveProperty("games", expect.any(Array))
+                    expect(response.body).toHaveProperty("access_token", expect.any(String))
                 })
         })
 
-        it("Should fetch category", () => {
+        it("Shouldnot fetch category (because there is not access token)", () => {
             return request(app)
                 .get("/category")
                 .then((response) => {
@@ -30,6 +32,7 @@ describe("Category", () => {
         it("Should fetch games all games based on category", () => {
             return request(app)
                 .get('/category/:categoryId')
+                .set("access_token", token)
                 .then(((response) => {
                     expect(response.status).toBe(200)
                     expect(response.body).toHaveProperty("imgUrl", expect.any(String))
@@ -37,13 +40,34 @@ describe("Category", () => {
                     expect(response.body).toHaveProperty("lvl", expect.any(String))
                     expect(response.body).toHaveProperty("categoryId", expect.any(Number))
                     expect(response.body).toHaveProperty("Category", expect.any(Object))
+                    expect(response.body).toHaveProperty("access_token", expect.any(String))
                     expect(response.body.Category).toHaveProperty("name", expect.any(String))
+                }))
+        })
+
+        it("Shouldnot fetch games all games based on category (not found)", () => {
+            return request(app)
+                .get('/category/:categoryId')
+                .set("access_token", token)
+                .then(((response) => {
+                    expect(response.status).toBe(404)
+                    expect(response.body).toHaveProperty("message", expect.any(String))
+                }))
+        })
+
+        it("Shouldnot fetch games all games based on category (because there is not access token)", () => {
+            return request(app)
+                .get('/category/:categoryId')
+                .then(((response) => {
+                    expect(response.status).toBe(404)
+                    expect(response.body).toHaveProperty("message", expect.any(String))
                 }))
         })
 
         it("Should fetch games on id", () => {
             return request(app)
                 .get('/games/:gamesId')
+                .set("access_token", token)
                 .then(((response) => {
                     expect(response.status).toBe(200)
                     expect(response.body).toHaveProperty("imgUrl", expect.any(String))
@@ -57,30 +81,36 @@ describe("Category", () => {
                     expect(response.body).toHaveProperty("optionD", expect.any(String))
                     expect(response.body).toHaveProperty("categoryId", expect.any(Number))
                     expect(response.body).toHaveProperty("Category", expect.any(Object))
+                    expect(response.body).toHaveProperty("access_token", expect.any(String))
                     expect(response.body.Category).toHaveProperty("name", expect.any(String))
                 }))
         })
 
-        it("Should fetch games on id (cannot access)", () => {
+        it("Shouldnot fetch games on id (cannot access)", () => {
             return request(app)
                 .get('/games/:gamesId')
-                .send({
-                    answer: "Jawaban B"
-                })
+                .set("access_token", token)
                 .then(((response) => {
                     expect(response.status).toBe(403)
                     expect(response.body).toHaveProperty("message", expect.any(String))
                 }))
         })
 
-        it("Should fetch games on id (not found)", () => {
+        it("Shouldnot fetch games on id (not found)", () => {
             return request(app)
                 .get('/games/:gamesId')
-                .send({
-                    answer: "Jawaban B"
-                })
+                .set("access_token", token)
                 .then(((response) => {
-                    expect(response.status).toBe(403)
+                    expect(response.status).toBe(404)
+                    expect(response.body).toHaveProperty("message", expect.any(String))
+                }))
+        })
+
+        it("Shouldnot fetch games on id (because there is not access token)", () => {
+            return request(app)
+                .get('/games/:gamesId')
+                .then(((response) => {
+                    expect(response.status).toBe(404)
                     expect(response.body).toHaveProperty("message", expect.any(String))
                 }))
         })
@@ -88,16 +118,32 @@ describe("Category", () => {
         it("Should answer games on id (right)", () => {
             return request(app)
                 .post('/games/:gamesId')
+                .set("access_token", token)
                 .send({
                     answer: "Jawaban A"
                 })
                 .then(((response) => {
                     expect(response.status).toBe(200)
                     expect(response.body).toHaveProperty("message", expect.any(String))
+                    expect(response.body).toHaveProperty("access_token", expect.any(String))
                 }))
         })
 
         it("Should answer games on id (wrong)", () => {
+            return request(app)
+                .post('/games/:gamesId')
+                .set("access_token", token)
+                .send({
+                    answer: "Jawaban B"
+                })
+                .then(((response) => {
+                    expect(response.status).toBe(200)
+                    expect(response.body).toHaveProperty("message", expect.any(String))
+                    expect(response.body).toHaveProperty("access_token", expect.any(String))
+                }))
+        })
+
+        it("Should answer games on id (because there is not access token)", () => {
             return request(app)
                 .post('/games/:gamesId')
                 .send({
